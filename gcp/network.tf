8396083688b964c91705ae2bd70e60c56f11e858
Matthias Wilhelm <ankertal@gmail.com>
2021-07-26 09:35:23 +0200

    Use gcp_name ist network.tf

--- gcp/network.tf
+++ gcp/network.tf
@@ -1,10 +1,10 @@
 resource "google_compute_network" "kbn_vpc" {
-  name                    = "kbn-vpc"
+  name                    =  "${var.gcp_name}-vpc"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "kbn_subnet" {
-  name          = "kbn-subnet"
+  name          = "${var.gcp_name}-subnet"
   ip_cidr_range = "10.0.0.0/24"
   network       = google_compute_network.kbn_vpc.id
 }
