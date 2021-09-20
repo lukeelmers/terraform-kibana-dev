cf3976cd4b2b417c9cf841be126ecaa3af26885d
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2021-09-20 17:58:11 +0200

    Change online server check to watch log

--- gcp/scripts/check_server_online.sh
+++ gcp/scripts/check_server_online.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-kibana_online=false
-while [ $kibana_online == false ]; do
-  content=$(curl -s http://elastic:changeme@localhost:5601/api/status)
-  state=$( jq -r  '.status.overall.state' <<< "${content}" )
-  if [[ $state == 'green' ]]
-  then
-    kibana_online=true
-  else
-    sleep 5
-  fi;
-done
+kibana_syslog=/var/log/syslog
+grep -m 1 "Kibana is now available" <(tail -f -n +1 $kibana_syslog)
