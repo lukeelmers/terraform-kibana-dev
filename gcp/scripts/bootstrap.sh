40bcb1cf472c8b8ae9066dc73a1c8c5dd0fc0da0
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2021-11-05 19:26:43 +0100

    Update bootstrap.sh

--- gcp/scripts/bootstrap.sh
+++ gcp/scripts/bootstrap.sh
@@ -6,24 +6,14 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" > /dev/null 2>&1 # This loads nvm
 
-
-cd ~/kibana
-# bootstrap kibana & run dev server
-nvm install
-npm install -g yarn
-
-# make node available for systemctl
-# taken from https://stackoverflow.com/questions/21215059/cant-use-nvm-from-root-or-sudo
-n=$(which node); \
-n=${n%/bin/node}; \
-chmod -R 755 $n/bin/*; \
-sudo cp -r $n/{bin,lib,share} /usr/local
-
 if [ -d "$HOME/eui" ]
 then
   echo "Building EUI package"
   # bootstrap kibana with the given EUI package
   cd ~/eui
+  nvm install
+  nvm use
+  npm install -g yarn
   yarn install
   yarn build
   npm pack
@@ -40,6 +30,9 @@ then
   echo "Building elastic-charts package"
   # bootstrap kibana with the given elastic chart package
   cd ~/elastic-charts
+  nvm install
+  nvm use
+  npm install -g yarn
   yarn install
   yarn build
   cd ~/elastic-charts/packages/charts
@@ -51,5 +44,18 @@ then
   sed -e "s/\"@elastic\/charts\": \"[0-9]*.[0-9]*.[0-9]*\"/\"@elastic\/charts\": \"..\/elastic-charts\/packages\/charts\/${package}\"/g" package.json > package.json.new
   mv -- package.json.new package.json
 fi
+
+cd ~/kibana
+# bootstrap kibana & run dev server
+nvm install
+nvm use
+npm install -g yarn
+
+# make node available for systemctl
+# taken from https://stackoverflow.com/questions/21215059/cant-use-nvm-from-root-or-sudo
+n=$(which node); \
+n=${n%/bin/node}; \
+chmod -R 755 $n/bin/*; \
+sudo cp -r $n/{bin,lib,share} /usr/local
 
 BUILD_TS_REFS_DISABLE=true yarn kbn bootstrap --no-validate
