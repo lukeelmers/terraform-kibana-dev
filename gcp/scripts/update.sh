b90c73a4bd53a1c68d767cf6958ab5ca3a73c999
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2021-11-19 21:44:26 +0100

    Allow different node versions for EUI and Elastic Charts (#9)

--- gcp/scripts/update.sh
+++ gcp/scripts/update.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
-echo "Pull latest changes from git repo"
+echo "Pull latest changes from kibana git repo"
 sudo systemctl stop elasticsearch
 sudo systemctl stop kibana
 cd ~/kibana || exit
 yarn kbn clean
+git stash
 git pull
-echo "Pull latest changes from git repo - successful"
+echo "Pull latest changes from kibana git repo - successful"
+cd ..
+
+if [ -d "$HOME/eui" ]
+then
+  echo "Pull latest changes from EUI git repo"
+  # bootstrap EUI with the given EUI package
+  cd ~/eui || exit
+  git stash
+  git pull
+  echo "Pull latest changes from EUI git repo - successful"
+  cd ..
+fi
+
+if [ -d "$HOME/elastic-charts" ]
+then
+  echo "Pull latest changes from Elastic charts git repo"
+  # bootstrap Elastic charts with the given EUI package
+  cd ~/elastic-charts || exit
+  git stash
+  git pull
+  echo "Pull latest changes from Elastic charts git repo - successful"
+  cd ..
+fi
